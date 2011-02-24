86f6a2f4980c815fc491eae1fb8f3812dff60610
Samuel Mullen <samullen@gmail.com>
2011-02-24 12:32:15 -0600

    Mostly functional...on linux, anyway
    - Library now can run four pomodoros with the accompanying five minute breaks.
    - Sends alerts to linux notify daemon
    - Progress bar shows how much time is left for iteration and break.

--- lib/tomahto/pomodoro.rb
+++ lib/tomahto/pomodoro.rb
@@ -4,26 +4,38 @@ module Tomahto
     end
 
     def run
-      progress "activity", 25
-      progress "break", 5
+      4.times do 
+        progress "activity", 25
+        notify "Tomahto: Iteration Complete!", "Take a short break and remember to add a mark to your activity."
+        progress "break", 5
+        notify "Tomahto: Break's over!", "On to the next pomodoro."
+      end
+      notify "You've completed a set. Take a longer break."
     end
 
     protected
 
     def progress(label, minutes)
-      (minutes * 60).times do |second|
+      1.upto(minutes * 60) do |second|
         display_progress(label, minutes, second)
         Kernel.sleep 1
       end
+      print "\n"
     end
 
     def display_progress(label, minutes, second)
-      progress_past = second / 60
-      progress_left = minutes - progress_past
-      seconds_left = minutes * 60 - second
-      minutes_left = seconds_left / 60
+      total_seconds_left = minutes * 60 - second
+      progress_past      = second / 60
+      progress_left      = minutes - progress_past
+      minutes_left       = total_seconds_left / 60
+      seconds_left       = total_seconds_left % 60
 
-      printf "\r%s - %s%s - %.2d:%.2d", label, "#" * progress_past, "." * progress_left, minutes_left, seconds_left % 60
+      printf "\r%20s - %s%s - %.2d:%.2d", label, "#" * progress_past, "." * progress_left, minutes_left, seconds_left
+    end
+
+    def notify(summary, message)
+      puts message
+      system("notify-send -t 500 \"#{summary}\" \"#{message}\"")
     end
   end
 
