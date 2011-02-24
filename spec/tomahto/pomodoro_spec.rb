78705b6bc7aacb234f712b8f9c499b520b3398c4
Samuel Mullen <samullen@gmail.com>
2011-02-24 08:20:41 -0600

    minor tweaks to specs just to get them set up.

--- spec/tomahto/pomodoro_spec.rb
+++ spec/tomahto/pomodoro_spec.rb
@@ -1,11 +1,17 @@
-$:.unshift File.dirname(__FILE__) + '/..'
 require 'spec_helper'
-require 'tomahto/pomodoro'
+require 'tomahto'
 
 describe Tomahto::Pomodoro do
-  context "#new" do
+  context "::new" do
     it 'instantiates a new Pomodoro instance' do
       Tomahto::Pomodoro.new.should be_an_instance_of Tomahto::Pomodoro
+    end
+  end
+
+  context "#start" do
+    it "runs for thirty minutes" do
+      tp = Tomahto::Pomodoro.new
+#       tp.run.should
     end
   end
 end
